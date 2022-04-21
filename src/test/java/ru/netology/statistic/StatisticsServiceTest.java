e45667a4b42535d32adab80964dc9cb434a3e86b
Maria Bychkova <mar.byckova@gmail.com>
2022-04-21 18:36:55 +0300

    Added top border test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -12,7 +12,17 @@ public class StatisticsServiceTest {
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+        long actual = service.findMax(incomesInBillions);
 
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxInTheEnd() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 13};
+        long expected = 13;
         long actual = service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
